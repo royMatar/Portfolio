c6e29c61aaa15a37f8d0746187e08f74a44b229a
royMatar <roy.mattar@outlook.com>
2024-08-17 03:29:05 +0300

    Chakraui added

--- src/app/components/ModelViewer.jsx
+++ src/app/components/ModelViewer.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls, useGLTF, Html } from '@react-three/drei';
 
 // Rotating Model Component
 function RotatingModel({ path }) {
@@ -15,16 +15,17 @@ function RotatingModel({ path }) {
   return <primitive object={scene} scale={4} />;
 }
 
+
 export default function ModelViewer() {
   return (
-    <div style={{ cursor: 'grab' }}>
+    <div style={{ cursor: 'grab', height:'65vh' }}>
       <Canvas 
-        camera={{ fov: 25, position: [2, 4, 10] }} 
-        style={{ height: '500px' }}
+        camera={{ fov: 25, position: [1, 4, 9] }} 
+        style={{ height: '100%' }}
       >
         <ambientLight intensity={1} />
         <directionalLight position={[0, 10, 5]} intensity={1} />
-        <Suspense fallback={null}>
+        <Suspense fallback={<Html>Loading...</Html>}>
           <RotatingModel path="/models/marble_bust_01_4k.glb" />
         </Suspense>
         <OrbitControls enableZoom={false} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 import ModelViewer from "../app/components/ModelViewer";
-import { color } from "three/webgpu";
+
 export default function Home() {
   return (
     <main className={styles.main}>
